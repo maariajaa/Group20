c44e7462ec9b68bdf9f97ca4d647f878d4d8b67a
maariajaa <mariajim2003@gmail.com>
2025-10-24 13:18:03 +0200

    Fix Assignment4

--- Assignment4/Maria_Jimenez_210030/task07.py
+++ Assignment4/Maria_Jimenez_210030/task07.py
@@ -1,14 +1,20 @@
+# %% [markdown]
 # **Task 07: Querying RDF(s)**
 
+# %%
+#!pip install rdflib
 import urllib.request
 url = 'https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2025-2026/refs/heads/master/Assignment4/course_materials/python/validation.py'
 urllib.request.urlretrieve(url, 'validation.py')
 github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2025-2026/master/Assignment4/course_materials"
 
+# %%
 from validation import Report
 
+# %% [markdown]
 # First let's read the RDF file
 
+# %%
 from rdflib import Graph, Namespace, Literal
 from rdflib.namespace import RDF, RDFS
 # Do not change the name of the variables
@@ -17,9 +23,11 @@ g.namespace_manager.bind('ns', Namespace("http://somewhere#"), override=False)
 g.parse(github_storage+"/rdf/data06.ttl", format="TTL")
 report = Report()
 
+# %% [markdown]
 # **TASK 7.1a: For all classes, list each classURI. If the class belogs to another class, then list its superclass.**
 # **Do the exercise in RDFLib returning a list of Tuples: (class, superclass) called "result". If a class does not have a super class, then return None as the superclass**
 
+# %%
 # TO DO
 # Visualize the results
 result = [] #list of tuples
@@ -37,16 +45,21 @@ for c in classes:
 for r in result:
   print(r)
 
+# %%
 ## Validation: Do not remove
 report.validate_07_1a(result)
 
+# %% [markdown]
 # **TASK 7.1b: Repeat the same exercise in SPARQL, returning the variables ?c (class) and ?sc (superclass)**
 
+# %%
 query =  """
+PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT ?c ?sc
 WHERE {
-  ?c rdf:type <http://www.w3.org/2000/01/rdf-schema#Class> .
-  OPTIONAL { ?c <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?sc . }
+  ?c rdf:type rdfs:Class .
+  OPTIONAL { ?c rdfs:subClassOf ?sc . }
 }
 ORDER BY ?c ?sc
 """
@@ -55,12 +68,15 @@ for r in g.query(query):
   print(r.c, r.sc)
 
 
+# %%
 ## Validation: Do not remove
 report.validate_07_1b(query,g)
 
+# %% [markdown]
 # **TASK 7.2a: List all individuals of "Person" with RDFLib (remember the subClasses). Return the individual URIs in a list called "individuals"**
 # 
 
+# %%
 ns = Namespace("http://oeg.fi.upm.es/def/people#")
 
 # variable to return
@@ -90,12 +106,17 @@ for cls in visited:
 for i in individuals:
   print(i)
 
+# %%
 # validation. Do not remove
 report.validate_07_02a(individuals)
 
+# %% [markdown]
 # **TASK 7.2b: Repeat the same exercise in SPARQL, returning the individual URIs in a variable ?ind**
 
+# %%
 query =  """
+PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT DISTINCT ?ind
 WHERE {
   ?ind rdf:type/rdfs:subClassOf* <http://oeg.fi.upm.es/def/people#Person> .
@@ -107,11 +128,14 @@ for r in g.query(query):
   print(r.ind)
 # Visualize the results
 
+# %%
 ## Validation: Do not remove
 report.validate_07_02b(g, query)
 
+# %% [markdown]
 # **TASK 7.3:  List the name and type of those who know Rocky (in SPARQL only). Use name and type as variables in the query**
 
+# %%
 # TO DO
 query =  """
 SELECT DISTINCT ?name ?type
@@ -134,11 +158,14 @@ for r in g.query(query):
   print(r.name, r.type)
 
 
+# %%
 ## Validation: Do not remove
 report.validate_07_03(g, query)
 
+# %% [markdown]
 # **Task 7.4: List the name of those entities who have a colleague with a dog, or that have a collegue who has a colleague who has a dog (in SPARQL). Return the results in a variable called name**
 
+# %%
 query =  """
 SELECT DISTINCT ?name
 WHERE {
@@ -163,6 +190,7 @@ ORDER BY ?name
 for r in g.query(query):
   print(r.name)
 
+# %%
 ## Validation: Do not remove
 report.validate_07_04(g,query)
 report.save_report("_Task_07")
